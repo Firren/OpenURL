1d6dd6cb6bb337f7d5753e7ab8d0aec555fdf920
David Johansson <david.johansson@instanthelp.se>
2010-12-23 00:18:35 +0000

    First upload

---
+++ OpenURL/OpenURL/BrowserPath.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.IO;
+
+namespace OpenURL
+{
+    class BrowserPath
+    {
+        /**
+         * <summary>
+         * Tries to find the location of a browsers exe file by its name
+         * </summary>
+         * <param name="appName">Browsers name</param>
+         * <returns>Path to browsers exe file</returns>
+         **/
+        public static string find(String appName)
+        {
+            string path = getInstallPathFromRegistry(appName);
+            if (path != null)
+            {
+                path += "\\" + getApplicationExe(path, appName);
+                return path;
+            }
+            return null;
+        }
+
+        
+
+        private static string getInstallPathFromRegistry(string pName)
+        {
+            string displayName;
+            RegistryKey key;
+
+            // search in: CurrentUser
+            key = Registry.CurrentUser.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+
+                if (displayName.ToLower().Contains(pName))
+                {
+                    return subkey.GetValue("InstallLocation") as String;
+                }
+            }
+
+            // search in: LocalMachine_32
+            key = Registry.LocalMachine.OpenSubKey(@"SOFTWARE\Microsoft\Windows\CurrentVersion\Uninstall");
+            foreach (String keyName in key.GetSubKeyNames())
+            {
+                RegistryKey subkey = key.OpenSubKey(keyName);
+                displayName = subkey.GetValue("DisplayName") as string;
+                if (displayName != null && displayName.ToLower().Contains(pName))
+                {
+                    return subkey.GetValue("InstallLocation") as String;
+                }
+            }
+
+
+
+            // NOT FOUND
+            return null;
+        }
+
+        private static string getApplicationExe(string path, string app)
+        {
+            DirectoryInfo directory = new DirectoryInfo(path);
+            FileInfo[] files = directory.GetFiles("*.exe");
+            foreach (FileInfo file in files)
+            {
+                if (file.Name.ToLower().Contains(app.ToLower()))
+                {
+                    return file.Name;
+                }
+            }
+            return null;
+        }
+
+
+        
+        private static string findByDisplayName(RegistryKey parentKey, string name)
+        {
+            //BrowserPath.findByDisplayName(Registry.LocalMachine.OpenSubKey(@"Software\Microsoft\Windows\CurrentVersion\App Paths\"), "iexplore.exe");
+            throw new NotImplementedException();
+            string[] nameList = parentKey.GetSubKeyNames();
+            for (int i = 0; i < nameList.Length; i++)
+            {
+                RegistryKey regKey = parentKey.OpenSubKey(nameList[i]);
+                try
+                {
+                    if (regKey.Name.ToString().ToLower().Contains(name))
+                    {
+                        return regKey.GetValue("").ToString();
+                    }
+                }
+                catch { }
+            }
+            return "";
+        }
+
+        
+    }
+}
---
+++ OpenURL/OpenURL/MainWindow.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+using System.Diagnostics;
+using System.ComponentModel;
+using Microsoft.Win32;
+using System.IO;
+namespace OpenURL
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+
+        private void button1_Click(object sender, RoutedEventArgs e)
+        {
+            string url = trimURL(urlInput.Text);
+            openInBrowser(url);
+        }
+
+        private void urlInput_KeyUp(object sender, KeyEventArgs e)
+        {
+            if (e.Key == Key.Enter)
+            {
+                string url = trimURL(urlInput.Text);
+                openInBrowser(url);
+            }
+        }
+
+        private static string trimURL(string url)
+        {
+            char[] trimChars = { '\n', '\r' };
+            url = url.Trim(trimChars);
+            return url;
+        }
+
+        private void openInBrowser(String url)
+        {
+            // Applications that we want to run, IE will be handled separately
+            string[] apps = { "chrome", "firefox", "safari", "opera" };
+
+
+            System.Diagnostics.Process[] proc = new System.Diagnostics.Process[apps.Length + 1];
+
+
+            /******************************* Run IE ************************************/
+            String path = Environment.GetFolderPath(Environment.SpecialFolder.ProgramFiles);
+            path = path + @"\Internet Explorer\iexplore.exe";
+            FileInfo file = new FileInfo(path);
+
+            if (file.Exists)
+            {
+                proc[apps.Length] = new System.Diagnostics.Process();
+                proc[apps.Length].StartInfo.FileName = path;
+                proc[apps.Length].StartInfo.Arguments = url;
+                try
+                {
+                    proc[proc.Length - 1].Start();
+                }
+                catch (Win32Exception ex)
+                {
+                }
+            }
+            /************************* Run everything else *****************************/
+
+            for (int i = 0; i < (apps.Length); i++)
+            {
+                try
+                {
+                    // First we need to locate our applications exe files
+                    path = BrowserPath.find(apps[i]);
+                    if (path != null)
+                    {
+                        // Create process and run application
+                        proc[i] = new System.Diagnostics.Process();
+                        proc[i].StartInfo.FileName = path;
+                        proc[i].StartInfo.Arguments = url;
+                        proc[i].Start();
+                    }
+                }
+                catch (Win32Exception ex)
+                {
+
+                }
+            }
+        }
+
+        private void Window_Loaded(object sender, RoutedEventArgs e)
+        {
+            urlInput.Focus();
+        }
+
+
+    }
+}
